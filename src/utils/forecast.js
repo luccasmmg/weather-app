817360d41a3f44a92ec747ae15b61863e944b9e1
Luccas Mateus <luccasmmg@gmail.com>
2019-06-13 15:47:37 -0300

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url = `https://api.darksky.net/forecast/92bbd7a7a1998f0e0d057810dfd91ab9/${latitude},${longitude}?lang=pt&units=si`;
+
+  request({ url, json: true}, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to service', undefined);
+    } else if (body.error) {
+      callback('Error in input', undefined);
+    } else {
+      callback(undefined, `${body.daily.data[0].summary} É atualmente ${body.currently.temperature} graus. Com uma chance de chuva de ${body.currently.precipProbability}%`);
+    }
+  });
+}
+
+module.exports = forecast
